824adb8f3fd995089405f66e52d2a8affbdec8a5
Dmytro Turskyi <dmytro.turskyi@gmail.com>
2024-03-03 10:39:26 -0500

    formatted code
    Signed-off-by: Dmytro Turskyi <dmytro.turskyi@gmail.com>

--- lib/weather_bloc_observer.dart
+++ lib/weather_bloc_observer.dart
@@ -18,8 +18,10 @@ class WeatherBlocObserver extends BlocObserver {
   }
 
   @override
-  void onTransition(Bloc<dynamic, dynamic> bloc,
-      Transition<dynamic, dynamic> transition,) {
+  void onTransition(
+    Bloc<dynamic, dynamic> bloc,
+    Transition<dynamic, dynamic> transition,
+  ) {
     super.onTransition(bloc, transition);
     log('onTransition $transition');
   }
